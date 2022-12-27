7dac7d486bf714da93d67c93ad75e4351498034c
Vanguard-Visuals <66173196+Vanguard-Visuals@users.noreply.github.com>
2022-12-27 20:53:14 +0000

    init

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAuKJj4-ibzCLMaVrVTeP3znXqbqDWD8cM",
+    authDomain: "netflix-clone-app-28f64.firebaseapp.com",
+    projectId: "netflix-clone-app-28f64",
+    storageBucket: "netflix-clone-app-28f64.appspot.com",
+    messagingSenderId: "891510472612",
+    appId: "1:891510472612:web:b14fbe9eac373f348c0b31",
+    measurementId: "G-Q1ZS8S29SD"
+  };
